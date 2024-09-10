00fd04e37959d87cc174db8d2ca3283f45c06499
Nicho Deng <nicho810d@gmail.com>
2024-09-10 17:53:30 +0800

    separate the func of UIs

--- XIAO-PowerBread-FW/XIAO-PowerBread-FW.ino
+++ XIAO-PowerBread-FW/XIAO-PowerBread-FW.ino
@@ -22,14 +22,16 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 float old_chA_v = 0, old_chA_a = 0, old_chA_w = 0;
 float old_chB_v = 0, old_chB_a = 0, old_chB_w = 0;
 
-#define color_Background 0x0000
-#define color_Text 0xFFFF
-#define color_ChannelA 0x055B
-#define color_ChannelB 0xF97C
-uint8_t chA_x = 1;// UI global variables
-uint8_t chA_y = 82;// UI global variables
-uint8_t chB_x = 1;// UI global variables
-uint8_t chB_y = 0;// UI global variables
+// #define color_Background 0x0000
+// #define color_Text 0xFFFF
+// #define color_ChannelA 0x055B
+// #define color_ChannelB 0xF97C
+// uint8_t chA_x = 1;// UI global variables
+// uint8_t chA_y = 82;// UI global variables
+// uint8_t chB_x = 1;// UI global variables
+// uint8_t chB_y = 0;// UI global variables
+
+#include "ui_functions.h"
 
 //Current sensor
 #include "INA3221Sensor.h"
@@ -52,137 +54,6 @@ int read_dial() {
 
 
 
-
-void ChannelInfoUpdate_A(float new_chA_v, float new_chA_a, float new_chA_w, float old_chA_v, float old_chA_a, float old_chA_w, uint16_t color = color_Text) {
-  uint8_t chA_1_x = chA_x + 4;
-  uint8_t chA_1_y = chA_y + 30;
-  uint8_t chA_2_y = chA_y + 50;
-  uint8_t chA_3_y = chA_y + 70;
-  uint8_t chA_v_x = chA_x + 70;
-  uint8_t chA_mA_x = chA_x + 64;
-  uint8_t chA_w_x = chA_x + 64;
-
-  int offset_defaultFont = -6;
-
-  tft.setFont(&FreeSansBold9pt7b);
-  tft.setTextSize(0);
-
-  // Update voltage if changed
-  if (new_chA_v != old_chA_v) {
-    updateChangedDigits(chA_1_x, chA_1_y, old_chA_v, new_chA_v, color);
-  }
-
-  // Update current if changed
-  if (new_chA_a != old_chA_a) {
-    updateChangedDigits(chA_1_x, chA_2_y, old_chA_a, new_chA_a, color);
-  }
-
-  // Update power if changed
-  if (new_chA_w != old_chA_w) {
-    updateChangedDigits(chA_1_x, chA_3_y, old_chA_w, new_chA_w, color);
-  }
-
-  // Print units (these don't need to be erased or updated)
-  tft.setFont();
-  tft.setTextColor(color);
-  tft.setCursor(chA_v_x, chA_1_y + offset_defaultFont);
-  tft.println("V");
-  tft.setCursor(chA_mA_x, chA_2_y + offset_defaultFont);
-  tft.println("mA");
-  tft.setCursor(chA_w_x, chA_3_y + offset_defaultFont);
-  tft.println("mW");
-}
-
-void ChannelInfoUpdate_B(float new_chB_v, float new_chB_a, float new_chB_w, float old_chB_v, float old_chB_a, float old_chB_w, uint16_t color = color_Text) {
-  uint8_t chB_1_x = chB_x + 4;
-  uint8_t chB_1_y = chB_y + 30;
-  uint8_t chB_2_y = chB_y + 50;
-  uint8_t chB_3_y = chB_y + 70;
-  uint8_t chB_v_x = chB_x + 70;
-  uint8_t chB_mA_x = chB_x + 64;
-  uint8_t chB_w_x = chB_x + 64;
-
-  int offset_defaultFont = -6;
-
-  tft.setFont(&FreeSansBold9pt7b);
-  tft.setTextSize(0);
-
-  // Update voltage if changed
-  if (new_chB_v != old_chB_v) {
-    updateChangedDigits(chB_1_x, chB_1_y, old_chB_v, new_chB_v, color);
-  }
-
-  // Update current if changed
-  if (new_chB_a != old_chB_a) {
-    updateChangedDigits(chB_1_x, chB_2_y, old_chB_a, new_chB_a, color);
-  }
-
-  // Update power if changed
-  if (new_chB_w != old_chB_w) {
-    updateChangedDigits(chB_1_x, chB_3_y, old_chB_w, new_chB_w, color);
-  }
-
-  // Print units (these don't need to be erased or updated)
-  tft.setFont();
-  tft.setTextColor(color);
-  tft.setCursor(chB_v_x, chB_1_y + offset_defaultFont);
-  tft.println("V");
-  tft.setCursor(chB_mA_x, chB_2_y + offset_defaultFont);
-  tft.println("mA");
-  tft.setCursor(chB_w_x, chB_3_y + offset_defaultFont);
-  tft.println("mW");
-}
-
-void updateChangedDigits(int x, int y, float oldValue, float newValue, uint16_t color) {
-  char oldStr[10], newStr[10];
-  char format[10];
-  
-  // Determine the number of decimal places automatically
-  int decimalPlaces = 3;  // Default to 3 decimal places
-  if (oldValue >= 10 || newValue >= 10) {
-    decimalPlaces = 2;
-  } else if (oldValue >= 100 || newValue >= 100) {
-    decimalPlaces = 1;
-  } else if (oldValue >= 1000 || newValue >= 1000) {
-    decimalPlaces = 0;
-  }
-  
-  snprintf(format, sizeof(format), "%%5.%df", decimalPlaces);
-  snprintf(oldStr, sizeof(oldStr), format, oldValue);
-  snprintf(newStr, sizeof(newStr), format, newValue);
-
-  tft.setFont(&FreeSansBold9pt7b);
-  tft.setTextSize(0);
-
-  // Get bounds of the entire string
-  int16_t x1, y1;
-  uint16_t w, h;
-  tft.getTextBounds(oldStr, x, y, &x1, &y1, &w, &h);
-
-  // Erase the entire old string
-  tft.fillRect(x1, y1, w, h, color_Background);
-
-  // Draw the new string
-  tft.setTextColor(color);
-  tft.setCursor(x, y);
-  tft.print(newStr);
-}
-
-void drawUIFramework() {
-  tft.drawRoundRect(chA_x, chA_y, 78, 78, 4, color_ChannelA);
-  tft.fillRoundRect(chA_x, chA_y, 65, 12, 4, color_ChannelA);
-
-  tft.drawRoundRect(chB_x, chB_y, 78, 78, 4, color_ChannelB);
-  tft.fillRoundRect(chB_x, chB_y, 65, 12, 4, color_ChannelB);
-
-  tft.setFont();
-  tft.setTextColor(color_Text);
-  tft.setTextSize(0);
-  tft.setCursor(chA_x + 5, chA_y + 2);
-  tft.println("Channel A");
-  tft.setCursor(chB_x + 5, chB_y + 2);
-  tft.println("Channel B");
-}
 
 
 
