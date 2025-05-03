b02dd392a127ea1ce428f5583f389cd0443f5680
Nicho Deng <nicho810d@gmail.com>
2025-05-03 14:06:30 +0800

    Update dialSwitch.cpp

--- Firmware/XPB-Firmware-PlatformIO/lib/dialSwitch/dialSwitch.cpp
+++ Firmware/XPB-Firmware-PlatformIO/lib/dialSwitch/dialSwitch.cpp
@@ -21,57 +21,62 @@ int DialFunction::readDialStatus() {
          * 150-250   | down    | 2     | ~204
          * 300-400   | up      | 1     | ~348
          */
-        int dial_threshold_min = 0;
-        int dial_threshold_down = 150;     // down ~204
-        int dial_threshold_press = 50;     // press ~116
-        int dial_threshold_up = 300;       // up ~348
-        int dial_threshold_max = 400;
+        int dial_threshold_reset_min = 0;
+        int dial_threshold_reset_max = 50;
+        int dial_threshold_press_min = 50;
+        int dial_threshold_press_max = 150;
+        int dial_threshold_down_min = 150;
+        int dial_threshold_down_max = 250;
+        int dial_threshold_up_min = 300;
+        int dial_threshold_up_max = 400;
     #elif defined(SEEED_XIAO_ESP32C3) || defined(SEEED_XIAO_ESP32S3)
-        /*
-         * Dial Analog Value Table on XIAO ESP32-C3 or XIAO ESP32-S3
-         * ----------------------
-         * Range     | Status  | Value | ADC Reading C3  | ADC Reading S3
-         * 0-100     | reset   | 0     | ~0              | ~0
-         * 100-200   | press   | 3     | ~159            | ~140
-         * 200-350   | down    | 2     | ~282            | ~247
-         * 350-600   | up      | 1     | ~490            | ~433
-         */
-        int dial_threshold_min = 0;
-        int dial_threshold_down = 200;      // down ~282
-        int dial_threshold_press = 100;     // press ~159  
-        int dial_threshold_up = 350;        // up ~490
-        int dial_threshold_max = 600;
+        #if defined(Proj_XIAOPowerMonitor)
+        int dial_threshold_reset_min = 0;
+        int dial_threshold_reset_max = 200;
+        int dial_threshold_press_min = 320;
+        int dial_threshold_press_max = 500;
+        int dial_threshold_down_min = 200;
+        int dial_threshold_down_max = 320;
+        int dial_threshold_up_min = 500;
+        int dial_threshold_up_max = 700;
+        #elif defined(Proj_XIAOPowerBread)
+        int dial_threshold_reset_min = 0;
+        int dial_threshold_reset_max = 100;
+        int dial_threshold_press_min = 100;
+        int dial_threshold_press_max = 200;
+        int dial_threshold_down_min = 200;
+        int dial_threshold_down_max = 350;
+        int dial_threshold_up_min = 350;
+        int dial_threshold_up_max = 600;
+        #endif
     #elif defined(SEEED_XIAO_RP2040) || defined(SEEED_XIAO_RP2350)
-        /*
-         * Dial Analog Value Table on XIAO RP2040 or XIAO RP2350
-         * ----------------------
-         * Range     | Status  | Value | ADC Reading RP2040  | ADC Reading RP2350
-         * 0-100     | reset   | 0     | ~0                  | ~0
-         * 100-200   | press   | 3     | ~146                | ~143
-         * 200-350   | down    | 2     | ~255                | ~252
-         * 350-600   | up      | 1     | ~436                | ~436
-         */
-        int dial_threshold_min = 0;
-        int dial_threshold_down = 200;      // down ~282
-        int dial_threshold_press = 100;     // press ~159  
-        int dial_threshold_up = 350;        // up ~490
-        int dial_threshold_max = 600;
+        int dial_threshold_reset_min = 0;
+        int dial_threshold_reset_max = 100;
+        int dial_threshold_press_min = 100;
+        int dial_threshold_press_max = 200;
+        int dial_threshold_down_min = 200;
+        int dial_threshold_down_max = 350;
+        int dial_threshold_up_min = 350;
+        int dial_threshold_up_max = 600;
     #endif
 
     int dialValue = read();
     
     int dialStatus_temp = 0;
-    if (dialValue >= dial_threshold_up && dialValue < dial_threshold_max) {
+    if (dialValue >= dial_threshold_up_min && dialValue < dial_threshold_up_max) {
         dialStatus_temp = 1;           // up
     }
-    else if (dialValue >= dial_threshold_down && dialValue < dial_threshold_up) {
+    else if (dialValue >= dial_threshold_down_min && dialValue < dial_threshold_down_max) {
         dialStatus_temp = 2;           // down
     }
-    else if (dialValue >= dial_threshold_press && dialValue < dial_threshold_down) {
+    else if (dialValue >= dial_threshold_press_min && dialValue < dial_threshold_press_max) {
         dialStatus_temp = 3;           // press
     }
-    else {
+    else if (dialValue >= dial_threshold_reset_min && dialValue < dial_threshold_reset_max) {
         dialStatus_temp = 0;           // reset
+    }
+    else {
+        dialStatus_temp = 0;           // invalid range, default to reset
     }   
 
     // Debug print
