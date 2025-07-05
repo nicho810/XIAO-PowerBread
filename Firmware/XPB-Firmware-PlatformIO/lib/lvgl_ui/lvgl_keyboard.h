3acc302cf1f742e6ba0511b31bbddb4a5d4fff8e
Nicho Deng <nicho810d@gmail.com>
2025-07-05 22:16:04 +0800

    update

--- Firmware/XPB-Firmware-PlatformIO/lib/lvgl_ui/lvgl_keyboard.h
+++ Firmware/XPB-Firmware-PlatformIO/lib/lvgl_ui/lvgl_keyboard.h
@@ -5,13 +5,9 @@
 #include "input_ButtonX3.h"
 #include "lvgl_uiManager.h"
 
-void setup_container_events(lv_obj_t *container);
-// LVGL Input Device Keyboard read function
-void keyboard_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
-
-void key_event_cb(lv_event_t *e);
-
-
+void setup_container_events(lv_obj_t *container); // Add event handling to the container
+void keyboard_read(lv_indev_drv_t *drv, lv_indev_data_t *data); // LVGL Input Device Keyboard read function
+void key_event_cb(lv_event_t *e); // LVGL key event callback function
 
 
 #endif
